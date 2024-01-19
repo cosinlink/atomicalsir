078a62b9b08b524bcb0c2e5fa13f6f467e1bbb95
Ethan <cosinlinker@gmail.com>
2024-01-19 18:07:07 +0800

    feat: modify retry cnt to 6

--- src/electrumx.rs
+++ src/electrumx.rs
@@ -105,7 +105,7 @@ pub trait Api: Config + Http {
 
 			tracing::info!("{} waiting for UTXO...", cnt);
 			cnt = cnt + 1;
-			if (cnt >= 3) {
+			if cnt >= 6 {
 				std::process::exit(0);
 			}
 			time::sleep(Duration::from_secs(5)).await;
